05918b890981640e279a91491d4ef685ea6dfcb6
rolewang <92693516+rolewang@users.noreply.github.com>
2021-10-17 20:52:07 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { useState } from "react";
-import colorData from "./color-data.json"
-import ColorList from "./components/ColorList"
-
+import colorData from "./color-data.json";
+import ColorList from "./components/ColorList";
+//some comment to see if it sticks
 function App() {
-  const [colors, setColors] = useState(colorData)
-  const [selectedStars, setSelectedStars] = useState(0)
+  const [colors, setColors] = useState(colorData);
+  const [selectedStars, setSelectedStars] = useState(0);
 
   function removeColor(id) {
-     const newColors = colors.filter(color => color.id !== id);
-     setColors(newColors)
+    const newColors = colors.filter((color) => color.id !== id);
+    setColors(newColors);
   }
   function rateColor(id, rating) {
-    console.log(colors)
-    const newColors = colors.map(color =>  color.id === id ? {...color, rating}: color)
-    setColors(newColors)
+    console.log(colors);
+    const newColors = colors.map((color) =>
+      color.id === id ? { ...color, rating } : color
+    );
+    setColors(newColors);
   }
   return (
     <>
-  
-    <ColorList colors={colors} onRemoveColor={removeColor} onRateColor={rateColor}/>
+      <ColorList
+        colors={colors}
+        onRemoveColor={removeColor}
+        onRateColor={rateColor}
+      />
     </>
   );
 }
